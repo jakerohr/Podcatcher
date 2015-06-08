3059effe632366325c797ed6ca0272a04a23ec4d
Jake Rohr <jakerohr@gmail.com>
2015-06-08 11:59:06 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# project_one
+# Podcatcher
 WDI Seattle Project One
 
 
 This is my first project for GA WDI. It is a podcatcher that grabs audio files based on search terms and the amount of time you have to listen.
 
 
-#Technologies used include: express, sequelize, bcrypt, node and postgres.
+##Technologies used include: express, sequelize, bcrypt, node and postgres.
 
-#I created this app in one week using npr's api and wimpy player. My sidebar was inspired by one created by maridlcrmn.
+##I created this app in one week using npr's api and wimpy player. My sidebar was inspired by one created by maridlcrmn.
 
 I have many more features I'd like to add including video and articles and more customizable search options and settings.
 
--- controllers/podcasts.js
+++ controllers/podcasts.js
@@ -17,12 +17,12 @@ router.get('/',function(req,res){
     }
 });
 
-
-
 router.get("/:id", function(req,res) {
   var term = req.query.term;
   var user = req.getUser();
   var userTime = req.query.userTime;
+
+  // use async to get more results
   var url = "http://api.npr.org/query?fields=title,teaser,show,audio,image&requiredAssets=audio&searchTerm=" + term + "&sort=relevance&output=JSON&numResults=50&apiKey="+ process.env.Npr_Key;
   request(url, function (error, response, data) {
     if (!error && response.statusCode == 200) {
@@ -33,13 +33,14 @@ router.get("/:id", function(req,res) {
     var playList = [];
 
       if (podcasts.list.story) {
+        // sort by episode length
         for (var i = 0; i < podcasts.list.story.length; i++) {
           var episodeLength = podcasts.list.story[i].audio[0].duration.$text;
           // check playlist length
           if (totalTime <= userTime) {
             // check episode length, add to playlist and add to total time
             if(episodeLength <= userTime * 60){
-              var file = podcasts.list.story[i].audio[0].format.mp4.$text
+              var file = podcasts.list.story[i].audio[0].format.mp4.$text //check for mp4 or other file
               var title = escape( podcasts.list.story[i].title.$text).replace(/%(..)/g,"&#x$1;")
               playList.push({length: episodeLength, title:  title, file: file.slice(0, file.indexOf('?'))});
               episodeData.push(podcasts.list.story[i]);
